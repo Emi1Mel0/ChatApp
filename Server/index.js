8434f4544c3160d42c56b928e04bd56b9cfbfb97
Emi1Mel0 <emilwagdy11@gmail.com>
2024-10-22 01:23:47 +0300

    adding the buffering and changing the port

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,15 @@
-const ws = require('ws')
+const ws = require("ws");
 
-const server = new ws.Server({port :'4000'})
+const server = new ws.Server({ port: "3000" });
 
-server.on('connection',socket=>{
-    socket.on('message',message=>{
-        console.log(message);
-        socket.send(`${message}`)
-        
-    })
-})
+server.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const b = Buffer.from(message);
+    console.log(b.toString());
+    socket.send(`${message}`);
+  });
+});
+
+// we receive message in buffer and if we want to read it
+// we need to change inside index.js
+// inside the server function , we add before log
